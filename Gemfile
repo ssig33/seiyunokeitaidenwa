3035f1cfae0ddb8172c3403d78e8af26856ba07b
ssig33 <mail@ssig33.com>
2013-04-07 11:10:35 +0900

    comment out groong

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '3.2.13'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
-gem 'rroonga', require: 'groonga'
+#gem 'rroonga', require: 'groonga'
 gem 'pg'
 gem 'sewell'
 gem 'thin'
--- config/initializers/groonga.rb
+++ config/initializers/groonga.rb
@@ -1,6 +1,6 @@
 Dir::mkdir ::Rails.root+"db/groonga" unless File.exist? ::Rails.root+"db/groonga"
-if File.exist? "#{::Rails.root}/db/groonga/search"
-  Groonga::Database.open "#{::Rails.root}/db/groonga/search"
-else
-  Groonga::Database.create path: "#{::Rails.root}/db/groonga/search"
-end
+#if File.exist? "#{::Rails.root}/db/groonga/search"
+#  Groonga::Database.open "#{::Rails.root}/db/groonga/search"
+#else
+#  Groonga::Database.create path: "#{::Rails.root}/db/groonga/search"
+#end
