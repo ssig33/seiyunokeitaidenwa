3e2768a5eb96613252ae2f3ef4e0fe59a7e1ffe5
ssig33 <mail@ssig33.com>
2013-04-07 12:21:04 +0900

    修正

--- db/migrate/20130407031424_change_type_on_keitaidenwas.rb
+++ db/migrate/20130407031424_change_type_on_keitaidenwas.rb
@@ -1,9 +1,9 @@
 class ChangeTypeOnKeitaidenwas < ActiveRecord::Migration
   def change
-    a = Seiyu.all.map{|x| [x.id, x.seiyu_id.to_s] }
+    a = Keitaidenwa.all.map{|x| [x.id, x.seiyu_id.to_s] }
     remove_column :keitaidenwas, :seiyu_id
     add_column :keitaidenwas, :seiyu_id, :integer
     add_index :keitaidenwas, :seiyu_id
-    a.each{|x| Seiyu.find(x.first).update_attributes(seiyu_id: x.last)}
+    a.each{|x| Keitaidenwa.find(x.first).update_attributes(seiyu_id: x.last)}
   end
 end
