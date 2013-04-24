789b0bcf657eead99c6142a6bd7d27ff2c91210b
ssig33 <mail@ssig33.com>
2013-04-24 15:27:18 +0900

    機種変更の間隔をつけた

--- app/models/seiyu.rb
+++ app/models/seiyu.rb
@@ -1,3 +1,4 @@
+#coding:utf-8
 class Seiyu < ActiveRecord::Base
   has_many :keitaidenwas
   attr_accessible :image, :name, :url
@@ -6,5 +7,14 @@ class Seiyu < ActiveRecord::Base
 
   def to_param
     self.name
+  end
+
+  def kankaku
+    if self.keitaidenwas.select('distinct phone').count < 2
+      '登録されている情報が少なく算出不可能'
+    else
+      ary = self.keitaidenwas.select('distinct phone').map{|x| self.keitaidenwas.where(phone: x.phone).order('id asc').first}.sort{|a,b| a.at <=> b.at}
+      " #{((ary.last.at - ary.first.at)/ary.count).to_i} 日"
+    end
   end
 end
