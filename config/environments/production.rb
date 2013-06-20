6d70d4083ea2b7156830438353f67c755462c7dc
ssig33 <mail@ssig33.com>
2013-06-20 12:50:35 +0900

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Seiyunokeitaidenwa::Application.configure do
   config.cache_classes = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.serve_static_assets = false
+  config.serve_static_assets = true
   config.assets.compress = true
   config.assets.compile = false
   config.assets.digest = true
